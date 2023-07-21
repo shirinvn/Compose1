688743ab36f7b0ca18ba70ace6aebacbceb18054
Vatani <shirinvatani97@gmail.com>
2023-07-21 13:59:47 +0330

    change in Condition

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,15 +22,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -40,6 +44,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
@@ -61,18 +66,24 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginForm() {
 
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var showError by remember { mutableStateOf(false) }
-    var isButtonEnabled by remember { mutableStateOf(true) }
-    val context = LocalContext.current
+
+    val isDataTrue by remember {
+        derivedStateOf {
+
+            username!="" && isUsernameValid(username) &&
+            password!="" && isPasswordValid(password)
+
+        }
+
+    }
 
 
-    Image(painter = painterResource(id = R.drawable.bg),
+    Image(painter = painterResource(id = R.drawable.bg2),
         contentDescription = "",
         contentScale = ContentScale.Crop,
         modifier = Modifier.fillMaxSize())
@@ -83,79 +94,105 @@ fun LoginForm() {
         .wrapContentSize(
             align = Alignment.Center
         ),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
+        UsernameText(username, onValueChange = {username=it}, "Username")
+        UserPasswordText(password, onValueChange = {password= it},"Password" )
 
+        UserButton(text = "Login", isEnabled=isDataTrue){
 
-        TextField(
-
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("username", color = Color(0xFFCCCCCC)) },
-            isError = showError && !isUsernameValid(username),
-            colors = TextFieldDefaults.textFieldColors(
-                disabledIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                containerColor = Color.White,
-                errorCursorColor = Color.Red,
-                errorIndicatorColor = Color.Red,
-                errorLabelColor = Color.Red
-            ),
-            leadingIcon = { Icon(imageVector = Icons.Filled.Person, contentDescription = "",
-                tint = Color(0xFFCCCCCC)
-            )} ,
-            shape = shapes.medium,
-
-
-            )
-
-
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            leadingIcon = { Icon(imageVector = Icons.Filled.Lock, contentDescription = "",
-                tint = Color(0xFFCCCCCC)
-            )} ,
-            label = { Text("password", color = Color(0xFFCCCCCC))} ,
-            isError = showError && !isPasswordValid(password),
-            colors = TextFieldDefaults.textFieldColors(
-                disabledIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                containerColor = Color.White,
-                errorCursorColor = Color.Red,
-                errorIndicatorColor = Color.Red,
-                errorLabelColor = Color.Red
-            ),
-            shape = shapes.medium,
-            singleLine = true,
-        )
-
-        Button(
-            onClick = {
-                showError = true
-                if (isUsernameValid(username) && isPasswordValid(password)) {
-                    Toast.makeText(context,"ورود موفقیت آمیز",Toast.LENGTH_SHORT).show()
-
-                }
-                Toast.makeText(context,"نام کاربری نباید با عدد شروع شود" +
-                        "    "+ "" +
-                        "رمز عبور باید دارای حروف انگلیسی بزرگ + کاراکتر + عدد باشد",Toast.LENGTH_SHORT).show()
-
-            },
-            enabled = isButtonEnabled,
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text("login")
         }
 
     }
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserPasswordText(value:String="", onValueChange: (String)-> Unit={}, label:String=""){
+
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange ,
+        leadingIcon = { Icon(imageVector = Icons.Filled.Lock, contentDescription = "",
+            tint = Color(0xFFCCCCCC)
+        )} ,
+        label = { Text(text = "password") } ,
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.White,
+            unfocusedIndicatorColor = Color.White,
+            containerColor = Color.Transparent,
+            textColor = Color.White,
+
+        ),
+        shape = shapes.medium,
+        singleLine = true,
+    )
+    if (value == "" ){
+        Text(text = "Password is required" , style = TextStyle(color = Color.Red))
+    } else if (!isPasswordValid(value)){
+        Text(text = "Password must be stronger" , style = TextStyle(color = Color.Red))
+    }
+
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UsernameText (value:String="", onValueChange: (String)-> Unit={}, label:String=""){
+
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange ,
+      label = { Text(label) },
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.White,
+            unfocusedIndicatorColor = Color.White,
+            containerColor = Color.Transparent,
+            textColor = Color.White,
+            unfocusedSupportingTextColor = Color.White,
+            errorLabelColor = Color.Red,
+
+        ),
+        leadingIcon = { Icon(imageVector = Icons.Filled.Person, contentDescription = "",
+            tint = Color(0xFFCCCCCC)
+        )} ,
+        shape = shapes.medium,
+    )
+    if (value == "" ){
+        Text(text = "Username is required" , style = TextStyle(color = Color.Red))
+    } else if (!isUsernameValid(value)){
+        Text(text = "Must not start with a number" , style = TextStyle(color = Color.Red))
+    }
+
+}
+
+@Composable
+fun UserButton(text:String="", isEnabled:Boolean= false, onButtonClicked:()-> Unit={}){
+
+
+    Button(
+        onClick = onButtonClicked,
+        modifier = Modifier.padding(top = 16.dp)
+        ,
+        enabled = isEnabled,
+        shape= shapes.medium,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+            disabledContainerColor = Color.Gray,
+            disabledContentColor = Color.White,
+
+        )
+    ) {
+        Text(text = text)
+    }
+}
 
 
 
@@ -166,10 +203,10 @@ private fun isUsernameValid(username: String): Boolean {
 }
 
 private fun isPasswordValid(password: String): Boolean {
+
     val regex = Regex("^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#\$%^&]).{6,}$")
     return regex.matches(password)
 }
-
 
 
 
